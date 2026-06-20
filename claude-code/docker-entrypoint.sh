1fc6962d6015069c1ac13a2b49bdac31d8606593
Yang Sheng Han <progamesigner@gmail.com>
2026-06-20 16:55:24 +0800

    Copy Claude Code configuration files when starting

--- claude-code/docker-entrypoint.sh
+++ claude-code/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+if [ -d $HOME/.claude ]; then
+    if [ -f $HOME/.claude.json ] && [ ! -L $HOME/.claude.json ]; then
+        mv -f $HOME/.claude.json $HOME/.claude/.claude.json
+    fi
+    ln -sf $HOME/.claude/.claude.json $HOME/.claude.json
+fi
+
 echo "Installing / Updating Claude Code..."
 curl -fsSL https://claude.ai/install.sh | bash
 
